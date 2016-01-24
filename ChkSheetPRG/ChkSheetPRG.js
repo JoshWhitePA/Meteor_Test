d09a82298f5fc08602f44f0515852fb7a518d681
JoshWhitePA <josh.white.pa@gmail.com>
2016-01-24 14:27:13 -0500

    Got it to work with an array and got it to output
    used this
    https://www.youtube.com/watch?v=nF5CRSEC8PA&list=PLLnpHn493BHECNl9I8gwos
    -hEfFrer7TV&index=5

--- ChkSheetPRG/ChkSheetPRG.js
+++ ChkSheetPRG/ChkSheetPRG.js
@@ -1,23 +1,20 @@
+
+
+CoreClass = new Mongo.Collection("coreClass");
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  // This code only runs on the client
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+    Template.body.helpers({
+        class:[{classID:"CSC-355",classDesc:"Talk about junk", creditHour:"3"}]
+    });
 }
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    // CoreClass.insert({ClassID:"1",ClassDesc:"stuff",CreditHour:"3"});
     // code to run on server at startup
   });
 }
